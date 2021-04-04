779ff9c2caf3dffc26c7b8c8e3e3b19b44922dce
Ricardo Delfin <me@rdelfin.com>
2021-04-04 20:21:38 +0100

    Added basic amethyst example

---
+++ src/state.rs
@@ -0,0 +1,43 @@
+use amethyst::{
+    core::transform::Transform,
+    input::{is_close_requested, is_key_down, VirtualKeyCode},
+    prelude::*,
+    renderer::Camera,
+    window::ScreenDimensions,
+};
+
+/// A dummy game state that shows 3 sprites.
+pub struct MyState;
+
+impl SimpleState for MyState {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+        let dimensions = (*world.read_resource::<ScreenDimensions>()).clone();
+        init_camera(world, &dimensions);
+    }
+
+    fn handle_event(
+        &mut self,
+        mut _data: StateData<'_, GameData<'_, '_>>,
+        event: StateEvent,
+    ) -> SimpleTrans {
+        if let StateEvent::Window(event) = &event {
+            if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
+                return Trans::Quit;
+            }
+        }
+
+        Trans::None
+    }
+}
+
+fn init_camera(world: &mut World, dimensions: &ScreenDimensions) {
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(dimensions.width() * 0.5, dimensions.height() * 0.5, 1.);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(dimensions.width(), dimensions.height()))
+        .with(transform)
+        .build();
+}
